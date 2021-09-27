208ecdb6f56724531433788d9c8e880af893ad20
Alex McClellan <69948740+amcclellan2001@users.noreply.github.com>
2021-09-27 09:00:31 -0400

    Created AlgorithmsProject.cpp

---
+++ AlgorithmsProject.cpp
@@ -0,0 +1,20 @@
+/*
+Zack, Collin, Steven, and Alex
+Dr. Williams
+Algorithms Project Code
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<array>
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
