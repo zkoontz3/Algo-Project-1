adbe656f169a8d52385877d86ab3e1ec2e9a7594
amcclellan2001 <amcclell@highpoint.edu>
2021-10-01 08:48:04 -0400

    Added random array generator

--- AlgorithmsProject.cpp
+++ AlgorithmsProject.cpp
@@ -12,7 +12,16 @@ using namespace std;
 
 int main()
 {
+    int randomArray[1000];
 
+    //Random Array Generator
+    srand(time(0));
+
+    for (int i = 0; i < 1000; i++)
+    {
+        int x = rand() % 1000;
+        randomArray[i] = x;
+    }
 
 
 
