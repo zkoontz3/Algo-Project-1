d6c68dd6d7de355c4b9891a270d4b3fc0ad4d4f1
zkoontz3 <69996962+zkoontz3@users.noreply.github.com>
2021-10-03 11:03:58 -0400

    Update AlgorithmsProject.cpp

--- AlgorithmsProject.cpp
+++ AlgorithmsProject.cpp
@@ -24,8 +24,75 @@ int main()
     }
 
 
+    
+//selection sort: Zach Koontz
+void selectionSort(int arr[], int n) 
+{
+    int i, j, min, tmp;    
+      for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++)
+          if (arr[j] < arr[min])
+              min = j;
+          if (min != i) {
+              tmp = arr[i];
+              arr[i] = arr[min];
+              arr[min] = tmp;
+    }
+  }
+}
 
+//Merge Function: Zach Koontz
+void merge(int arr[], int start, int mid, int end)
+{
+   int length1 = mid - start + 1;
+   int length2 = end - mid;  
 
+    int leftArr[length1], rightArr[length2];
 
+  for (int i = 0; i < length1; i++)
+    leftArr[i] = arr[start + i];
+  for (int j = 0; j < length2; j++)
+    rightArr[j] = arr[mid + 1 + j];
+
+  int a, b, c;
+  a = 0;
+  b = 0;
+  c = start;
+
+  while (a < length1 && b < length2) {
+    if (leftArr[a] <= rightArr[b]) {
+      arr[c] = leftArr[a];
+      a++;
+    } else {
+      arr[c] = rightArr[b];
+      b++;
+    }
+    c++;
+  }
+
+  while (a < length1) {
+    arr[c] = leftArr[a];
+    a++;
+    c++;
+  }
+
+  while (b < length2) {
+    arr[c] = rightArr[b];
+    b++;
+    c++;
+  }
+}
+
+//Merge Sort: Zach Koontz
+void mergeSort(int arr[], int start, int end)
+{
+  if(start < end){
+     int mid = start + (end - start)/2;
+     mergeSort(arr,start,mid);
+     mergeSort(arr,mid + 1,end);
+     merge(arr, start, mid, end);
+  }
+}
     return 0;
 }
