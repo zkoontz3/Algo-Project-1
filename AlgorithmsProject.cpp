5cbbc4d7063951e6c9e0a5980b3cbfe12eebe2d8
cmccarri <46940896+cmccarri@users.noreply.github.com>
2021-10-06 08:50:25 -0400

    First commit

--- AlgorithmsProject.cpp
+++ AlgorithmsProject.cpp
@@ -6,8 +6,9 @@ Algorithms Project Code
 
 #include<iostream>
 #include<cstdlib>
+#include<stdio.h>
 #include<array>
-#include<timercpp>
+
 
 using namespace std;
 
@@ -16,7 +17,8 @@ void swap(int *x, int *p);
 void selectionSort(int arr[], int n);
 void merge(int arr[], int start, int mid, int end);
 void mergeSort(int arr[], int start, int end);
-
+int partition (int arr[], int low, int high);
+void quickSort(int arr[], int low, int high);
 //Heap Sort and Bubble Sort: Alex McClellan
 //Heap Sort functions from GeeksForGeeks.com 
 void heapify(int arr[], int heapSize, int i);
@@ -217,4 +219,30 @@ void exchangeSort(int arr[], int z)
 void insertionSort()
 {
     
+}
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    
+    int i = (low - 1);  
+    for (int j = low; j <= high- 1; j++)
+    {
+        
+        if (arr[j] <= pivot)
+        {
+            i++;    
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
 }
